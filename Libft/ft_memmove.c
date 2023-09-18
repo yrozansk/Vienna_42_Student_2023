e3e217f8f8c41022f6e684f478f5af725e1a540b
Yogesvara Rozanski <yrozansk@c2r4p6.42vienna.com>
2023-09-18 19:37:17 +0200

    Changes in ft_memmove.c

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: yrozansk <yrozansk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 19:12:05 by yrozansk          #+#    #+#             */
-/*   Updated: 2023/09/18 14:58:29 by yrozansk         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:35:52 by yrozansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,34 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
+	int				z;
+	int				i;
 	unsigned char	*ptrdest;
 	unsigned char	*ptrsrc;
 
 	ptrdest = (unsigned char *) dest;
 	ptrsrc = (unsigned char *) src;
+	z = n;
 	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	if (n > 0)
+	if (ptrdest > ptrsrc)
 	{
-		while (i < n && (char)ptrsrc[i] != '\0')
+		z--;
+		while (z >= 0)
 		{
-			ptrdest[i] = (char)ptrsrc[i];
-			i++;
+			(ptrdest[z]) = (ptrsrc[z]);
+			z--;
 		}
 	}
+	if (ptrdest < ptrsrc)
+	{
+		while (i < n)
+		{
+			(ptrdest[i]) = (ptrsrc[i]);
+			i++;
+		}
+	}	
 	return (dest);
 }
 /*
@@ -46,4 +57,3 @@ int	main (void)
 	
 	return(0);
 }*/
-// it seems not to be ready
