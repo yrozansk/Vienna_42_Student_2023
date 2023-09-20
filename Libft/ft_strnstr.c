f8128c8a0b2e651b96403bb536407980f434aab3
Yogesvara Rozanski <yrozansk@c2r4p6.42vienna.com>
2023-09-20 14:58:25 +0200

    changes

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrozansk <yrozansk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 20:16:49 by yrozansk          #+#    #+#             */
+/*   Updated: 2023/09/20 14:57:18 by yrozansk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <bsd/string.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	c;
+
+	i = 0;
+	c = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (c < len && big[c])
+	{
+		while (c < len && little[i] && big[c] && little[i] == big[c])
+		{
+			i++;
+			c++;
+		}
+		if (little[i] == '\0')
+			return ((char *)&big[c - i]);
+		c = c - i + 1;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	const char *big = "Foo Bar Baz";
+	const char *little = "Bar";
+	char *result1;
+	char *result2;
+
+	result1 = strnstr(big, little, 9);
+	result2 = ft_strnstr(big, little, 9);
+	printf("%s\n", result1);
+	printf("%s\n", result2);
+	return (0);
+}*/
+// Not Ready (PS: cc with "-lbsd" Flag &
+// use "#include<bsd/string.h>". Do the same with "strnstr" )
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yrozansk <yrozansk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 13:51:39 by yrozansk          #+#    #+#             */
-/*   Updated: 2023/09/18 14:53:25 by yrozansk         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:57:45 by yrozansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ size_t			ft_strlcat(char *dst, const char *src, size_t size);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 int				ft_strlen(char *str);
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
+char			*ft_strnstr(const char *big, const char *little, size_t len)
 char			*ft_strrchr(const char *str, int c);
 int				ft_tolower(char a);
 int				ft_toupper(char c);
