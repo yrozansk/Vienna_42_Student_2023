aa7417f2fe3d4db06457da4bcf99e785d1185b2f
Yogesvara Rozanski <yrozansk@c2r4p6.42vienna.com>
2023-09-21 14:15:38 +0200

    changes

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,13 +6,32 @@
 /*   By: yrozansk <yrozansk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:59:51 by yrozansk          #+#    #+#             */
-/*   Updated: 2023/09/20 17:58:58 by yrozansk         ###   ########.fr       */
+/*   Updated: 2023/09/21 14:15:12 by yrozansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
+#include "libft.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void			*str;
+	unsigned char	*ptrstr;
+	
+	if ((unsigned long long) nmemb * size > 18446744073709551615ULL)
+		return (malloc(0));
+	if (nmemb == 0)	
+		return (malloc(0));
+	str = malloc (nmemb * size);
+	if (str == NULL)
+		return (NULL);
+	ptrstr = (unsigned char *)str;
+	ft_bzero(ptrstr, nmemb * size);
+	return (ptrstr);
+}
+/*
 int main () {
 	int i, n;
 	int *a;
@@ -20,7 +39,7 @@ int main () {
 	printf("Number of elements to be entered:");
 	scanf("%d",&n);
 
-	a = (int*)calloc(n, sizeof(int));
+	a = (int*)ft_calloc(n, sizeof(int));
 	printf("Enter %d numbers:\n",n);
 	for( i=0 ; i < n ; i++ ) {
 		scanf("%d",&a[i]);
@@ -33,4 +52,4 @@ int main () {
 	free(a);
 
 	return(0);
-}
+}*/
