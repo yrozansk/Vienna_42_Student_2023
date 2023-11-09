96aea2dfa8c6e07dac7a563aa0915d00c1c11b28
yrozansk <144276355+yrozansk@users.noreply.github.com>
2023-11-09 13:32:10 +0000

    one fix in this function

--- ft_printf/ft_print_adress.c
+++ ft_printf/ft_print_adress.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_adress.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrozansk <yrozansk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:05:08 by yrozansk          #+#    #+#             */
-/*   Updated: 2023/11/07 14:21:12 by yrozansk         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:31:00 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_print_adress(unsigned long long int m, int *len)
 
 	n = (unsigned long long int) m;
 	hexa = "0123456789abcdef";
-	if (n > 16)
+	if (n >= 16)
 	{
 		ft_print_adress(n / 16, len);
 		ft_print_adress(n % 16, len);
